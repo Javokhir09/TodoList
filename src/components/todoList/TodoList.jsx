b774b8a5d5360faaa42915a39e45a211dcec4110
Javohir Zed <javokhirturdikulov@gmail.com>
2024-10-27 13:48:54 +0500

    Style improvement

--- src/components/todoList/TodoList.jsx
+++ src/components/todoList/TodoList.jsx
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from 'react'
+import unchecked from "../../assets/images/unchecked.png"
+import checked from "../../assets/images/checked.png"
+import icon from "../../assets/images/icon.png"
 
 function TodoList() {
   const [todos, setTodos] = useState([]);
@@ -26,17 +29,23 @@ function TodoList() {
     const updateTodos = todos.map((todo, i) =>
       i === index ? {...todo, completed: !todo.completed} : todo);
     setTodos(updateTodos);
-  }
+  };
 
   const deleteTodo = (index) => {
     const updateTodos = todos.filter((_, i) => i !== index);
     setTodos(updateTodos);
-  }
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      addTodo();
+    };
+  };
 
   return (
     <div className='todo-con'>
       <div className="todo-top">
-        <h3>Todo List</h3>
+        <h3>Todo List <img src={icon} alt="icon" width={50} /></h3>
         <hr />
         <div className="bottom">
 
@@ -46,6 +55,7 @@ function TodoList() {
             placeholder='Add new todo...'
             value={newTodo}
             onChange={(e) => setNewTodo(e.target.value)}
+            onKeyDown={handleKeyDown}
           />
 
           <button type='submit' onClick={addTodo}>Add</button>
@@ -56,17 +66,24 @@ function TodoList() {
         {todos.map((todo, index) => (
           <li key={index}>
 
-            <span
-              onClick={() => {toggleComplete(index)}} 
-              style={{
-                cursor: 'pointer',
-                textDecoration: todo.completed ? 'line-through': 'none'
-              }}
-            >
-              {todo.text}
-            </span>
-
-            <button onClick={() => deleteTodo(index)}>&times;</button>
+            <div className="left-side">
+              <img 
+                src={todo.completed ? checked : unchecked} alt="" width={25} 
+                onClick={() => {toggleComplete(index)}} 
+              />
+              <span
+                className='text'
+                onClick={() => {toggleComplete(index)}} 
+                style={{
+                  cursor: 'pointer',
+                  textDecoration: todo.completed ? 'line-through': 'none'
+                }}
+                >
+                {todo.text}
+              </span>
+            </div>
+          
+            <span className='del' onClick={() => deleteTodo(index)}>&times;</span>
 
           </li>
         ))}
